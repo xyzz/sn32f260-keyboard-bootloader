3e951bd610e86d6cabc3d4c812508db4fc521c08
Ilya Zhuravlev <whatever@xyz.is>
2020-02-08 11:30:56 -0500

    Add ganss gk87pro

--- src/config.h
+++ src/config.h
@@ -8,6 +8,12 @@
 #define BLKEY_INPUT_PIN 1
 #define BLKEY_OUTPUT_GPIO SN_GPIO2
 #define BLKEY_OUTPUT_PIN 8
+#elif defined(TARGET_GANSS_GK87PRO)
+/* Spacebar */
+#define BLKEY_INPUT_GPIO SN_GPIO2
+#define BLKEY_INPUT_PIN 8
+#define BLKEY_OUTPUT_GPIO SN_GPIO0
+#define BLKEY_OUTPUT_PIN 12
 #else
 #error Not configured for this target.
 #endif
